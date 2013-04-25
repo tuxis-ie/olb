199e1d4acac190a571d158af487ba554ac4861f4
Mark Schouten <mark@tuxis.nl>
2013-04-25 18:30:37 +0200

    We can now create rrds for cpu, swap, memory and interfaces

--- olb/olb-stats.py
+++ olb/olb-stats.py
@@ -41,9 +41,12 @@ sysfetch['swap']['ds']['free']        = 'GAUGE'
 sysfetch['ifaces'] = {}
 sysfetch['ifaces']['oid'] = {}
 sysfetch['ifaces']['ds'] = {}
-sysfetch['ifaces']['oid']['ilist']    = '.1.3.6.1.2.1.2.2.1.2'
-sysfetch['ifaces']['oid']['oin']      = '.1.3.6.1.2.1.31.1.1.1.6'
-sysfetch['ifaces']['oid']['oout']     = '.1.3.6.1.2.1.31.1.1.1.10'
+sysfetch['ifaces']['oid']['ilist']    = '.1.3.6.1.2.1.2.2.1.1'
+sysfetch['ifaces']['oid']['names']    = '.1.3.6.1.2.1.2.2.1.2'
+sysfetch['ifaces']['oid']['oin']      = '.1.3.6.1.2.1.2.2.1.10'
+sysfetch['ifaces']['oid']['oout']     = '.1.3.6.1.2.1.2.2.1.16'
+sysfetch['ifaces']['ds']['oin']       = 'COUNTER'
+sysfetch['ifaces']['ds']['oout']      = 'COUNTER'
 
 RRDDIR = "rrd"
 
@@ -67,58 +70,35 @@ def create_rrd(filename, args):
 
 for stype in sysfetch:
     rrd = ""
-    values = []
-    values.append(now)
     if stype in [ 'cpu', 'memory', 'swap' ]:
+        values = []
+        values.append(now)
         rrd = os.path.join(RRDDIR, stype+'.rrd')
         if os.path.isfile(rrd) == False:
-            create_rrd(rrd, sysfetch[stype])
             print "Create %s" % (rrd)
+            create_rrd(rrd, sysfetch[stype])
 
         myRRD = RRD(rrd)
         for s in sysfetch[stype]['oid']:
-            r = netsnmp.snmpget(sysfetch[stype]['oid'][s], Version=2, DestHost='localhost', Community='public')
+            r = netsnmp.snmpget(sysfetch[stype]['oid'][s], Version=1, DestHost='localhost', Community='public')
             values.append(str(r[0]))
 
         myRRD.bufferValue(':'.join(values))
         myRRD.update()
+    if stype == "ifaces":
+        for i in netsnmp.snmpwalk(sysfetch[stype]['oid']['ilist'], Version=1, DestHost='localhost', Community='public'):
+            values = []
+            values.append(now)
+            name = netsnmp.snmpget(sysfetch[stype]['oid']['names']+'.'+i, Version=1, DestHost='localhost', Community='public')[0]
+            oin  = netsnmp.snmpget(sysfetch[stype]['oid']['oin']+'.'+i, Version=1, DestHost='localhost', Community='public')[0]
+            oout = netsnmp.snmpget(sysfetch[stype]['oid']['oout']+'.'+i, Version=1, DestHost='localhost', Community='public')[0]
+            rrd = os.path.join(RRDDIR, '_'.join([stype, name])+'.rrd')
+            if os.path.isfile(rrd) == False:
+                print "Create %s" % (rrd)
+                create_rrd(rrd, sysfetch[stype])
 
-#filename = '/tmp/test.rrd'
-#
-#if os.path.isfile(filename) == False:
-#    dataSources = []
-#    roundRobinArchives = []
-#    dataSources.append(
-#        DataSource( dsName='user', dsType='GAUGE', heartbeat=600))
-#    dataSources.append(
-#        DataSource( dsName='system', dsType='GAUGE', heartbeat=600))
-#    dataSources.append(
-#        DataSource( dsName='idle', dsType='GAUGE', heartbeat=600))
-#    
-#    roundRobinArchives.append(RRA(cf='AVERAGE', xff=0.5, steps=1, rows=288))
-#    roundRobinArchives.append(RRA(cf='AVERAGE', xff=0.5, steps=12, rows=744))
-#    roundRobinArchives.append(RRA(cf='AVERAGE', xff=0.5, steps=24, rows=1116))
-#    roundRobinArchives.append(RRA(cf='AVERAGE', xff=0.5, steps=48, rows=2191))
-#    myRRD = RRD(
-#        filename, ds=dataSources, rra=roundRobinArchives, start=int(now)-60)
-#    myRRD.create()
-#else:
-#    myRRD = RRD(filename)
-#
-#r = []
-#r.append(now)
-#
-#myRRD.bufferValue(':'.join(r))
-#myRRD.update()
-#
-##
-##  515  snmpwalk -v2c -c public localhost .1.3.6.1.4.1.2021.11.9.0
-##    516  snmpwalk -v2c -c public localhost .1.3.6.1.4.1.2021.11.10.0
-##      517  snmpwalk -v2c -c public localhost .1.3.6.1.4.1.2021.11.11.0
-##
-#
-##         RRA:AVERAGE:0.5:1:288 \
-##         RRA:AVERAGE:0.5:12:31
-##                         24:93
-##                         48:183
-#
+            values.append(oin)
+            values.append(oout)
+            myRRD = RRD(rrd)
+            myRRD.bufferValue(':'.join(values))
+            myRRD.update()
